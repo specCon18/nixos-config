6c57fa546bde769ccce068257bb34b96ffa947d6
arouzing <owner@arouzing.xyz>
2023-02-04 00:40:50 -0500

    minimal groups

--- .modules/users/arouzing.nix
+++ .modules/users/arouzing.nix
@@ -2,11 +2,11 @@
 {
     users.users.arouzing = {
     isNormalUser = true;
-    initialPassword = "password~!@~";
+    initialHashedPassword = "$6$tucSnzN8mqHQo/Fd$Q/RtaTpoXN0xnlLAFy6ohWWYuTYd54CXaCrocV1vgFRQVuONga1LyzwdJ0vXa.NT6MRcO7IXNQ3YeURJsSdP61";
     openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJAGm66rJsr8vjRCYDkH4lEPncPq27o6BHzpmRmkzOiM"
     ];
     description = "admin";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    extraGroups = [ "wheel" "docker" ];
   };
 }
