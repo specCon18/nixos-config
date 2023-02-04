8bfc9f63d5836973f2b77ca44a2690dae37b06dc
arouzing <owner@arouzing.xyz>
2023-02-04 00:42:37 -0500

    inital commit for open ldpa

---
+++ hosts/openldap-docker.nix
@@ -0,0 +1,40 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+        # ../.modules/base/hardware.nix
+        ../.modules/services/docker.nix
+        ../.modules/users/arouzing.nix
+        ../.modules/user/speccon18.nix
+        ../.modules/services/openssh.nix
+    ];
+
+    # base packages
+    environment.systemPackages = with pkgs; [
+      htop
+      vim
+      # sleep
+      tailscale
+    ];
+
+  networking = {
+    firewall.checkReversePath = "loose";
+    hostName = "openldap"; # Define your hostname.
+    # networkmanager.enable = true;
+  };
+
+  # services.tailscale.enable = true;
+
+  time.timeZone = "America/New_York";
+
+  # Open ports in the firewall.
+  networking.firewall = { 
+    enable = true;
+    allowedTCPPorts = [];
+    allowedUDPPorts = [];
+  };
+  ## main services
+  system.stateVersion = "22.11";
+
+}
