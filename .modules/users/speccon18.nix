6fa0b4ce443a6fab2010a6d5275fe6b6df275b9a
Steven <specCon18@gmail.com>
2023-03-25 23:11:27 -0400

    updated password hash

--- .modules/users/speccon18.nix
+++ .modules/users/speccon18.nix
@@ -3,7 +3,7 @@
   users.users.speccon18 = {
     shell = pkgs.zsh;
     isNormalUser = true;
-    initialHashedPassword = "$6$pJB0TDUj8IS8hQNJ$GfENlHg89lsUjRiSaePWJeqX1pevTTZOuEw5KgcVEpyPw9lyiAifz5ZiuOQnYxUAMhAiCmF/pCjaWSy6m5sWM/";
+    initialHashedPassword = "$y$j9T$RdLBHOvUpb17egl0d16LT/$3Y2RD/tT1IZ0nkfAR13pp3IzBjvKLRgGpDPLobUeO23";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILdigmndcdQD/864P059K2hZOXyEkbGvMkH0/b2QavkD speccon18@creatorforge"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPc1AQ6hcjdSZZuhS5SISwtulFoTLpC5f5JoMFQkZ5l2 specCon18@DESKTOP-Q1I2PAE"
