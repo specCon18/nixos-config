b84cb458587f55f555a4c10910f4bd3480470ce8
Steven <specCon18@gmail.com>
2023-03-26 22:38:50 -0400

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 Disko Partiton Scheme, Disk to Partition, Flake Name"
+    exit 1
+fi
+
 nix-env -iA nixos.git
 git clone https://git.skdevstudios.cloud/specCon18/nixos-config.git
-sudo nix run github:nix-community/disko -- --mode zap_create_mount /modules/disko/luks-lvm.nix --arg disks '[ "/dev/nvme0n1" ]'
+sudo nix run github:nix-community/disko -- --mode zap_create_mount /modules/disko/"$1" --arg disks '[ "'"$2"'" ]'
 nixos-generate-config --no-filesystems --root /mnt
-nixos-install --flake ./#creatorforge-framework 
+nixos-install --flake ./#"$3"
