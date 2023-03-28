a7bb33bc24832654b27b00a6043879d7886f3b67
Steven <specCon18@gmail.com>
2023-03-28 18:25:28 -0400

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
 if [ $# -ne 3 ]; then
-    echo "Usage: $0 Disko Partiton Scheme, Disk to Partition, Flake Name"
+    echo \
+"Usage: 
+    \$1 Disko Partiton Scheme
+        - This pulls from existing paritions located in ./modules/disko/ inside the repo
+    \$2 Disk to Partition
+        - This script can and will destro data, please do be careful 
+    \$3 Target system flake
+        - These can be found via running nix show.
+"
     exit 1
 fi
 
