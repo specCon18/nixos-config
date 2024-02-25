ac1a0e7e42d7a71e14190bfb9f852083158edbca
specCon18 <steven.carpenter@skdevstudios.com>
2024-02-25 01:18:51 -0500

    fixed budgie wierdness

--- modules/home-manager/vscode.nix
+++ modules/home-manager/vscode.nix
@@ -5,29 +5,6 @@
       package = pkgs.vscode.fhs;
       enableExtensionUpdateCheck = true;
       enableUpdateCheck = false;
-      extensions = with pkgs.vscode-extensions; [
-        bbenoist.nix
-        redhat.vscode-yaml
-        bungcip.better-toml
-        firefox-devtools.vscode-firefox-debug
-        ms-vscode-remote.remote-ssh
-        ms-azuretools.vscode-docker
-        editorconfig.editorconfig
-        dbaeumer.vscode-eslint
-        donjayamanne.githistory
-        github.copilot
-        eamodio.gitlens
-        graphql.vscode-graphql
-        oderwat.indent-rainbow
-        skellock.just
-        jnoortheen.nix-ide
-        christian-kohler.path-intellisense
-        esbenp.prettier-vscode
-        svelte.svelte-vscode
-        bradlc.vscode-tailwindcss
-        thenuprojectcontributors.vscode-nushell-lang
-        matklad.rust-analyzer
-      ];
       userSettings = {
         "workbench.colorTheme" = "Monokai Pro (Filter Octagon)";
         "workbench.startupEditor" = "none";
